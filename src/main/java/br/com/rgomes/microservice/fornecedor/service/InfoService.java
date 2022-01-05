eea636c78ceb6d264bfa5af5e3dd985e41be3337
Rodolfo Gomes <rodolfoaguiargomes@gmail.com>
2022-01-05 15:28:09 -0300

    fist commit

---
+++ src/main/java/br/com/rgomes/microservice/fornecedor/service/InfoService.java
@@ -0,0 +1,17 @@
+package br.com.rgomes.microservice.fornecedor.service;
+
+import br.com.rgomes.microservice.fornecedor.model.InfoFornecedor;
+import br.com.rgomes.microservice.fornecedor.repository.InfoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InfoService {
+
+    @Autowired
+    private InfoRepository infoRepository;
+
+    public InfoFornecedor getInfoPorEstado(String estado) {
+        return infoRepository.findByEstado(estado);
+    }
+}
